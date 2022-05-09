bbca669bcc98d92718cd0cf4790bab35c755f55d
omarbackup <104847568+omarbackup@users.noreply.github.com>
2022-05-09 21:17:56 +0200

    Create minimize.s

---
+++ man/minimize.s
@@ -0,0 +1,36 @@
+bits 32
+ALR  db AL
+AHR  db AH
+AXR  db AX
+ESR  db ES
+SPR  db SP
+BXR  db BX
+ESPR db ESP
+BLR  db BL
+BHR  db BH
+CLR  db CL
+CHR  db CH
+CXR  db CX
+BPR  db BP
+
+MOV AL, 0h
+MOV AH, 0h
+MOV AX, 0h
+MOV ES, 0h
+MOV SP, 0h
+MOV BX, 0h
+MOV BL, 0h
+MOV BH, 0h
+MOV CL, 0h
+MOV CH, 0h
+MOV CX, 0h
+MOV ESP, 0h
+MOV BP, 0h
+
+;  (c) Tobey Organization 
+;        The Medioka project.
+;        5/9/2022 9:17 PM
+
+
+
+
