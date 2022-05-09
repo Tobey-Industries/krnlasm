6d9b9c8b3817b80061e4838532b671a1d844db55
omarbackup <104847568+omarbackup@users.noreply.github.com>
2022-05-09 19:59:41 +0200

    Update refresh.s

--- func/refresh.s
+++ func/refresh.s
@@ -2,7 +2,9 @@ spr db sp
 alr db al
 mov al, 0h
 mov sp, 0h
+mov al, alr
+mov sp, spr
 ;
 ; (c) Tobey Organization
 ;     The Medioka project.
-;        5/7/2022  /  8:50
+;        5/9/2022  /  7:59
